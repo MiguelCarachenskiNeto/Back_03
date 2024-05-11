0b1deef5e7e48f867b94b8ca09a9278ed8212ca0
MiguelCarachenskiNeto <miguel.carachenski.neto@escola.pr.gov.br>
2024-05-10 21:35:38 -0300

    first commit

---
+++ server1.js
@@ -0,0 +1,13 @@
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+    console.log("Efetuado o pedido");
+    res.setHeader("content-type", "text/html")
+    res.write('<h1>Pedido aceito</h1>')
+    res.write('<h2>Já entendi que o pedido foi aceito</h2>')
+    res.end();
+})
+
+server.listen(3000, 'localhost', ()=>{
+    console.log("Servidor iniciado");
+})
